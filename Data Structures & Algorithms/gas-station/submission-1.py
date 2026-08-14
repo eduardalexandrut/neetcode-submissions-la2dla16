7abeddb73ee3dc95053c0b3b0c3351c8c994166b
Eduard Toni Alexandru <eduardtoni.alexandru@studio.unibo.it>
2026-08-14 13:51:55 +0200

    Add: gas-station - submission-1

---
+++ Data Structures & Algorithms/gas-station/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        if sum(gas) < sum(cost):
+            return -1
+
+        current_tank = 0
+        start = 0
+
+        for i in range(len(gas)):
+            current_tank += gas[i] - cost[i]
+
+            if current_tank < 0:
+                current_tank = 0
+                start = i + 1
+
+        return start
